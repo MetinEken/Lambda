e721329f4edec54847c543996d00c369b4d464b5
MetinEken <metineken2021@gmail.com>
2022-06-06 10:48:45 +0200

    Lamda01

---
+++ src/Day01/Lamda01.java
@@ -0,0 +1,18 @@
+package Day01;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lamda01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        System.out.println(8*8);
+
+        List<Integer> liste = Arrays.asList(1,2,3,4,5);
+    liste.forEach(x -> System.out.print(x+" "));
+
+    }
+}
